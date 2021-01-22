72ed3c2429d65de7a6f4c8292755b7eb60ffd96a
Alina Paukova <a.paukova@innopolis.university>
2021-01-22 19:12:39 +0300

    first 3 lectures

--- src/renderer/rasterizer/rasterizer_renderer.cpp
+++ src/renderer/rasterizer/rasterizer_renderer.cpp
@@ -5,7 +5,19 @@
 
 void cg::renderer::rasterization_renderer::init()
 {
-	THROW_ERROR("Not implemented yet");
+	// Load model
+	model = std::make_shared<cg::world::model>();
+	model->load_obj(settings->model_path);
+
+	// Create render target
+	render_target = std::make_shared<cg::resource<cg::unsigned_color>>(
+		settings->width, settings->height);
+
+	// Create rasterizer
+	rasterizer =
+		std::make_shared<cg::renderer::rasterizer<cg::vertex, cg::unsigned_color>>();
+	rasterizer->set_render_target(render_target);
+	rasterizer->set_vertex_buffer(model->get_vertex_buffer());
 }
 
 void cg::renderer::rasterization_renderer::destroy() {}
@@ -14,5 +26,6 @@ void cg::renderer::rasterization_renderer::update() {}
 
 void cg::renderer::rasterization_renderer::render()
 {
-	THROW_ERROR("Not implemented yet");
+	rasterizer->clear_render_target({ 255, 255, 0 });
+	cg::utils::save_resource(*render_target, settings->result_path);
 }
