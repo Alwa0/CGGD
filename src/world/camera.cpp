e6dd59f651a137a31e9e7b0abd953d01aeab546f
Vadim Koreshkov <evilgod-2@mail.ru>
2021-01-22 14:27:18 +0300

    first 3 lectures

--- src/world/camera.cpp
+++ src/world/camera.cpp
@@ -59,9 +59,15 @@ void cg::world::camera::set_z_far(float in_z_far)
 
 const float4x4 cg::world::camera::get_view_matrix() const
 {
+	float3 up{ 0.f, 1.f, 0.f };
 	float3 eye = position + get_direction();
 	float3 z_axis = normalize(position - eye);
-	return float4x4();
+	float3 x_axis = normalize(cross(up, z_axis));
+	float3 y_axis = cross(z_axis, x_axis);
+	return float4x4{ { x_axis.x, x_axis.y, x_axis.z, 0 },
+					 { y_axis.x, y_axis.y, y_axis.z, 0 },
+					 { z_axis.x, z_axis.y, z_axis.z, 0 },
+					 { -dot(x_axis, position), -dot(y_axis, position), - dot(z_axis, position), 1 } };
 }
 
 #ifdef DX12
@@ -80,8 +86,10 @@ const DirectX::XMMATRIX cg::world::camera::get_dxm_projection_matrix() const
 
 const float4x4 cg::world::camera::get_projection_matrix() const
 {
-	THROW_ERROR("Not implemented yet");
-	return float4x4{};
+	float f = 1.f / std::tanf(angle_of_view / 2.f);
+	return float4x4{
+	
+	};
 }
 
 const float3 cg::world::camera::get_position() const
@@ -92,8 +100,8 @@ const float3 cg::world::camera::get_position() const
 
 const float3 cg::world::camera::get_direction() const
 {
-	THROW_ERROR("Not implemented yet");
-	return float3{};
+	return float3{ std::sin(theta) * std::cos(phi), std::sin(phi),
+					-std::cos(theta) *std::cos(phi) };
 }
 
 const float3 cg::world::camera::get_right() const
